8f9335671f79dbee62692b05459d67c775e7533c
Jitendra Rathod <rjitendrarathod@gmail.com>
2023-01-25 21:07:49 +0530

    Fix-Remove Close and next and preveus option

--- frappe/public/js/frappe/form/toolbar.js
+++ frappe/public/js/frappe/form/toolbar.js
@@ -286,22 +286,22 @@ frappe.ui.form.Toolbar = class Toolbar {
 	make_navigation() {
 		// Navigate
 		if (!this.frm.is_new() && !this.frm.meta.issingle) {
-			this.page.add_action_icon(
-				"left",
-				() => {
-					this.frm.navigate_records(1);
-				},
-				"prev-doc",
-				__("Previous Document")
-			);
-			this.page.add_action_icon(
-				"right",
-				() => {
-					this.frm.navigate_records(0);
-				},
-				"next-doc",
-				__("Next Document")
-			);
+			// this.page.add_action_icon(
+			// 	"left",
+			// 	() => {
+			// 		this.frm.navigate_records(1);
+			// 	},
+			// 	"prev-doc",
+			// 	__("Previous Document")
+			// );
+			// this.page.add_action_icon(
+			// 	"right",
+			// 	() => {
+			// 		this.frm.navigate_records(0);
+			// 	},
+			// 	"next-doc",
+			// 	__("Next Document")
+			// );
 		}
 	}
 
